a1a919ed5ad6b5141d8e06c036203d7508c790a2
Andrea Berrones <31523761+andygarza96@users.noreply.github.com>
2019-09-16 00:55:21 -0600

    Carros no funcional

---
+++ models/carros.model.js
@@ -0,0 +1,129 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const carroSchema = new Schema({
+    model: { type: String, required: true },
+    color: { type: String, required: true },
+    licensePlate: { type: String, required: true },
+    houseNumber: {type: Number, required: true, default: 0},
+    year: {type: Number, require: true}
+  });
+
+/**
+ * Class for the Car model.
+ */
+class CarroClass {
+    constructor() {
+    }
+    setHouseNumber(newHouse){
+        if(this.debts = [] && typeof(newHouse) === 'number'){
+            if (this.houseNumber !== newHouse) {
+                this.houseNumber = newHouse;
+            }
+        } else {
+            throw new Error('No se puede cambiar de residencia si se tiene deudas pendientes');
+        }
+    }
+
+    setFirstName(newFirstName){
+        if(typeof(newFirstName) === 'string'){
+            this.fistName = newFirstName;
+        } else {
+            throw new Error('Ingresa un nombre válido');
+        }
+    }
+
+    setLastName(newLastName){
+        if(typeof(newLastName) === 'string'){
+            this.fistName = newLastName;
+        } else {
+            throw new Error('Ingresa un nombre válido');
+        }
+    }
+    
+    setPhoneNumber(newNumber) {
+        if(typeof(newNumber) === 'number'){
+            this.cellphone = newNumber;
+        } else {
+            throw new Error('Ingresa un número de telefono valido');
+        } 
+    }
+
+
+    setHouseNumber(newNumber) {
+        if(typeof(newNumber) === 'number'){
+            this.houseNumber = newNumber;
+        } else {
+            throw new Error('Ingresa un número de casa valido');
+        }
+    }
+
+    saveResident() {
+        if(this.houseNumber !== 0 || this.fistName !== "" || this.lastName !== "" || this.cellphone !== 0 ) {
+            return true;
+        } else {
+            throw new Error('¡Debes de tener todos los datos obligatorios!');
+        }
+    }
+
+    deleteResident() {
+        if(this.is_deleted === false) {
+            if(this.debts.length === 0 && this.cars.length === 0 && this.houseNumber === 0){
+                if (this.firstName !== "" || this.lastName !== "" || this.cellphone !== 0) {
+                    this.is_deleted = true;   
+                } else {
+                    throw new Error("¡No puedes eliminar un residente vacio!");
+                }
+            } else {
+                throw new Error("¡No puedes eliminar un residente con datos vinculados!");
+            }
+        } else {
+            throw new Error("¡No puedes eliminar un residente que ya ha sido eliminado!");
+        }
+    }
+
+    getDebts() {
+        return this.debts;
+    }
+
+    deleteDebt(debt){
+        if(this.debts !== []){
+            let index = this.debts.indexOf(debt);
+            this.debts.splice(index, 1);
+        } else {
+            throw new Error('No se puede eliminar una deuda que no existe')
+        }
+    }
+
+    deleteCar(car){
+        if(this.cars !== []){
+            let index = this.cars.indexOf(car);
+            this.cars.splice(index, 1);
+        } else {
+            throw new Error('No se puede eliminar un carro que no existe')
+        }
+    }
+
+    debug(){
+        console.log("Nombre = "+this.firstName);
+        console.log("Apellido = "+this.lastName);
+        console.log("Celular = "+this.cellphone);
+        console.log("Casa = "+this.houseNumber);
+        console.log("Deudas = "+this.debts);
+        console.log("Carros = "+this.cars);
+        console.log("¿Eliminado? = "+this.is_deleted);
+        
+    }
+
+    
+
+}
+
+//Load class
+carroSchema.loadClass(CarroClass);
+
+let CartModel = mongoose.model('Car', carroSchema);
+
+module.exports = {
+    CarModel
+}
--- test/carros.test.js
+++ test/carros.test.js
@@ -8,66 +8,113 @@ const CarModel = require('../models/carros.model').CarModel;
 mongoose.promise = Promise;
 mongoose.connect('mongodb://127.0.0.1:27014/frac');
 
-describe('RegistroAutos',function(){
+describe.skip('Autos',function(){
     //Funcionalidad de modificar Actividad
     describe('Registrar Auto',function(){
+        it('No debe de tener nulos');
+        it('Deberia de guardarse');
+            let newCar = newCarModel({
+                model:'Derby',
+                color: 'rojo',
+                licensePlate: 'EML-LS-8565',
+                houseNumber: 34,
+                year: 2005
+            });
         
-         describe('Dar de Alta Año',function(){
-             it('Número de 4 dígitos');
-             it('No puede ser nulo');
-        });
-         describe('Dar de Alta color',function(){
-             it('tipo String');
-             it('No puede ser nulo');
-        
-         });
-         describe('Dar de Alta Modelo',function(){
-             it('Tiene que ser de tipo String');
-             it('No puede ser nulo');
-         });
+            describe('Dar de Alta Año',function(){
+                var year = (currentTime.getFullYear() + 3); 
 
-         describe('Dar de Alta Placa',function(){
-            it('Tiene que ser de tipo String');
-            it('No puede ser nulo');
+                it('No puede ser mayor del año actual +2');
+                it('No puede ser nulo');
+                it('tiene que ser un numero');
+                
+                
+                expect().to.be.a("number");
+                expect().to.be.not.deep.equal(null);
+                expect().to.be.below(year);
+            });
+            describe('Dar de Alta color',function(){
+                it('tipo String');
+                it('No puede ser nulo');
+
+                expect().to.be.a("string");
+                expect().to.be.not.deep.equal(null);
+            });
+            describe('Dar de Alta Modelo',function(){
+                it('Tiene que ser de tipo String');
+                it('No puede ser nulo');
+
+                expect().to.be.a("string");
+                expect().to.be.not.deep.equal(null);   
+            });
+
+            describe('Dar de Alta Placa',function(){
+                it('Tiene que ser de tipo String');
+                it('No puede ser nulo');
+
+                expect().to.be.not.deep.equal(null); 
+                //Comparacion con el regex error   
+                
+            });
             
-        });
+            describe('Asignar casa',function(){
+                it('No puede ser nulo');
+                it('tiene que ser un numero');
+            
+                expect().to.be.not.deep.equal(null);   
+                expect().to.be.a("number"); 
+            });
+            
+          
         
-        describe('Asignar casa',function(){
-            it('Tiene que existir en el registro de las casas');
-            it('No puede ser nulo');
-            
+    
         });
-         
- 
-    });
     
     describe('Modificar Auto',function(){
-        
+        //Debe de existit un registro 
         describe('Modificar Año',function(){
-            it('Número de 4 dígitos');
+            var year = (currentTime.getFullYear() + 3); 
+            it('no puede ser mayor del año actual +3');
             it('No puede ser nulo');
-            //Limite de fechas
+
+            expect().to.be.a("number");
+            expect().to.be.not.deep.equal(null);
+            expect().to.be.below(year);
+            
        });
         describe('Modificar color',function(){
             it('tipo String');
             it('No puede ser nulo');
+
+            expect().to.be.a("string");
+            expect().to.be.not.deep.equal(null);
        
         });
         describe('Modificar Modelo',function(){
             it('Tiene que ser de tipo String');
             it('No puede ser nulo');
+
+            expect().to.be.a("string");
+            expect().to.be.not.deep.equal(null);
         });
 
         describe('Modificar Placa',function(){
            it('Tiene que ser de tipo String');
            it('No puede ser nulo');
            //Agregar un REGEX para validar placa
+           expect().to.be.a("string");
+           expect().to.be.not.deep.equal(null);
+           
            
        });
        
        describe('Modificar casa',function(){
-           it('Tiene que existir en el registro de las casas');
            it('No puede ser nulo');
+
+           expect().to.be.a("number");
+           expect().to.be.not.deep.equal(null);
+
+           
            
        });      
 
@@ -75,7 +122,23 @@ describe('RegistroAutos',function(){
 
 
    describe('Eliminar Auto',function(){
+       it('Tiene que existir');
+       it('No puede existir despues de eliminarse');
+
+
 
     });
+
+    describe('Cambiar Auto',function(){
+        it('Tiene que existir');
+        it('no puede haber campos nulos');
+        it('Año no puede ser mayor al actual +2');
+        it('Color tiene que ser string');
+        it('Modelo tiene que ser String');
+        it('La placa debe de coincidir con el regex');
+
+        
+ 
+     });
 
 });
